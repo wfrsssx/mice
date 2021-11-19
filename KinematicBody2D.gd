b1713c6ab43564dc091709f2b7274d0dd5a7dadb
ilia <wfrsssx@gmail.com>
2021-11-20 02:34:48 +0400

    re-init

---
+++ KinematicBody2D.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const MOTION_SPEED = 160 # Pixels/second.
+
+onready var _animated_sprite = $AnimatedSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	_animated_sprite.play("idle_sw")
+	var motion = Vector2()
+	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	motion.y /= 2
+	motion = motion.normalized() * MOTION_SPEED
+	#warning-ignore:return_value_discarded
+	move_and_slide(motion)
