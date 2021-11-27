4fffba583bc63b27e01029c354026bef00bbf802
ilia <wfrsssx@gmail.com>
2021-11-27 21:46:01 +0400

    add test lvl and some anims

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,21 +1,47 @@
 extends KinematicBody2D
 
-const MOTION_SPEED = 160 # Pixels/second.
+const MOTION_SPEED = 110 # Pixels/second.
 
 onready var _animated_sprite = $AnimatedSprite
+var anim_dir = "sw"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+func get_move_anim(motion: Vector2) -> String:
+	var curr_anim = "idle"
+	
+	if motion.length() == 0:
+		curr_anim = "idle"
+	else:
+		curr_anim = "run"
+		anim_dir = ""
+		if motion.y > 0:
+			anim_dir = "s"
+		elif motion.y < 0:
+			anim_dir = "n"
+			
+		if motion.x > 0:
+			anim_dir += "w"#"e"
+		elif motion.x < 0:
+			anim_dir += "w"
+						
+	return curr_anim + "_" + anim_dir
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	_animated_sprite.play("idle_sw")
 	var motion = Vector2()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	motion.y /= 2
 	motion = motion.normalized() * MOTION_SPEED
-	#warning-ignore:return_value_discarded
-	move_and_slide(motion)
+	
+	var curr_anim = get_move_anim(motion)
+	
+	if motion.length() > 0:
+		_animated_sprite.set_flip_h(motion.x > 0)
+	
+	_animated_sprite.play(curr_anim)
+	
+	motion = move_and_slide(motion)
